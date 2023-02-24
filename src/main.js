8bcae902279b84c8c27a231bbf3eb00811111f20
Timazlo <timazloy04444@gmail.com>
2023-02-24 23:05:50 +0300

    upd: vulidate

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App'
 import router from "./router";
+import useVuelidate from "@vuelidate/core";
 
 const app = createApp(App);
 
 
 app.use(router)
+app.use(useVuelidate)
 app.mount('#app')
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,14 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="onSubmit">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input
             id="email"
             type="text"
-            class="validate"
+            v-model.trim="email"
+            :class="{invalid: ($v.email.$dirty && $v.email.required)}"
+
         >
         <label for="email">Email</label>
         <small class="helper-text invalid">Email</small>
@@ -34,15 +36,42 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
 
 <script>
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+
 export default {
-  name: 'vLogin'
+  name: 'vLogin',
+  data: () => ({
+    email: '',
+    password: '',
+    v$: useVuelidate,
+  }),
+  validations: () => ({
+    email: { required, email },
+    password: {required, minLength: minLength((6))},
+  }),
+  methods: {
+    onSubmit() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        console.log(123)
+        return
+      }
+      this.$router.push('/')
+    }
+  },
+  mounted() {
+    console.log(required)
+    console.log(email)
+    console.log(minLength)
+  }
 }
 </script>
 
