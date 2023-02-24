ea04e89dc313fe9fe2849693e2ee4af2fab3b123
Timazlo <timazloy04444@gmail.com>
2023-02-24 21:15:52 +0300

    upd: header functional

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app111">
+  <div id="app">
     <component :is="layout">
       <router-view/>
     </component>
--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,7 +5,7 @@
         <a href="#" @click.prevent="$emit('toggle')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{date}}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -14,6 +14,7 @@
               class="dropdown-trigger black-text"
               href="#"
               data-target="dropdown"
+              ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +22,13 @@
 
           <ul id='dropdown' class='dropdown-content'>
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -37,13 +38,57 @@
     </div>
   </nav>
 </template>
-
 <script>
+import M from 'materialize-css'
+// import dateFilter from "@/filters/date.filter";
+
 export default {
-  name: "vNavbar"
+  name: "vNavbar",
+  data: () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null,
+    options: {},
+  }),
+  methods: {
+    logout() {
+      console.log('logout')
+      this.$router.push('/login?message=logout')
+    },
+  },
+  computed: {
+    dateFilter() {
+      const options = {}
+
+      options.day = '2-digit'
+      options.month = 'long'
+      options.year = 'numeric'
+      options.hour = '2-digit'
+      options.minute = '2-digit'
+      options.second = '2-digit'
+
+      return new Intl.DateTimeFormat('ru-RU', options).format(new Date())
+    },
+
+  },
+  created() {
+    this.date = this.dateFilter
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date = this.dateFilter
+    }, 1000)
+
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {constrainWidth: true})
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy()
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
