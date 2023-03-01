66a366e2eff734c051b30232f51430b267311c39
Timazlo <timazloy04444@gmail.com>
2023-03-02 00:54:44 +0300

    add: firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
 import { createApp } from 'vue'
 import App from './App'
 import router from "./router";
+import store from './store'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-const app = createApp(App);
 
-app.use(router)
-app.mount('#app')
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAT_D1nADQfQpyBK3qzxjhdhZzV6nyeyB4",
+    authDomain: "vue3-crm-33170.firebaseapp.com",
+    projectId: "vue3-crm-33170",
+    storageBucket: "vue3-crm-33170.appspot.com",
+    messagingSenderId: "491182782434",
+    appId: "1:491182782434:web:a651af3b4dc443677e6bbb",
+    measurementId: "G-4X05Z15NC6"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+
+firebase.auth().onAuthStateChanged(() => {
+    const app = createApp(App);
+    app.use(router)
+    app.use(store)
+    app.mount('#app')
+})
