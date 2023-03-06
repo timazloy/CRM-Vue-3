ab5ea6dcae99a7f2871605bab735a0c985559f2d
Tima <timazloy04444@gmail.com>
2023-03-06 18:14:25 +0300

    fix: errors

--- src/layouts/EmptyLayout.vue
+++ src/layouts/EmptyLayout.vue
@@ -16,12 +16,7 @@ export default {
   },
   watch: {
     error(fbError) {
-      const regForError = /(?<=\()[^)]+(?=\))/g
-      // const error = String(fbError).match(regForError) === null ? 'other' : String(fbError).match(regForError)[0]
-
-      const error = String(fbError).match(regForError)[0]
-      // toastError(messages[error])
-      toastError(messages[error] || 'Что-то пошло не так')
+      toastError(messages[fbError.code] || 'Что-то пошло не так')
     }
   }
 }
